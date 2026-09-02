451a718366e01ebca4b6b44360cbc011b5fe4456
Rezoan Shakil Prince <prince.cuet.77@gmail.com>
2026-09-02 20:39:10 +0600

    feat: handle not found route & global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
+import { notFound } from './middlewares/notFound';
+import { globalErrorHandler } from './middlewares/globalErrorHandler';
 
 const app: Application = express();
 
@@ -13,5 +15,9 @@ app.use(cookieParser());
 app.get('/', (req: Request, res: Response) => {
   res.send('Developer Assessment Platform is running...');
 });
+
+app.use(notFound);
+
+app.use(globalErrorHandler);
 
 export default app;
